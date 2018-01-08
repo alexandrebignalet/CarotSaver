247b1468f1dbd90afdae0b26c99a7978ba23d553
FCCAxl <pipajim64@gmail.com>
2018-01-08 14:55:14 +0100

    Wires menu component to app

--- src/main/webapp/app/carot-saver/menu/menu.component.ts
+++ src/main/webapp/app/carot-saver/menu/menu.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+import { Subscription } from 'rxjs/Rx';
+import { JhiEventManager, JhiParseLinks, JhiPaginationUtil, JhiAlertService } from 'ng-jhipster';
 
-import { Account, LoginModalService, Principal } from '../../shared';
+import {MenuCs} from "../../entities/menu/menu-cs.model";
+import {MenuCsService} from "../../entities/menu/menu-cs.service";
+import { ITEMS_PER_PAGE, Principal, ResponseWrapper, Account, LoginModalService } from '../../shared';
 
 @Component({
     selector: 'jhi-home',
@@ -12,37 +15,48 @@ import { Account, LoginModalService, Principal } from '../../shared';
     ]
 
 })
-export class MenuComponent implements OnInit {
-    account: Account;
-    modalRef: NgbModalRef;
+export class MenuComponent implements OnInit, OnDestroy {
+    menus: MenuCs[];
+    currentAccount: any;
+    eventSubscriber: Subscription;
 
     constructor(
-        private principal: Principal,
-        private loginModalService: LoginModalService,
-        private eventManager: JhiEventManager
+        private menuService: MenuCsService,
+        private jhiAlertService: JhiAlertService,
+        private eventManager: JhiEventManager,
+        private principal: Principal
     ) {
     }
 
+    loadAll() {
+        this.menuService.query().subscribe(
+            (res: ResponseWrapper) => {
+                this.menus = res.json;
+                console.log(this.menus);
+            },
+            (res: ResponseWrapper) => this.onError(res.json)
+        );
+    }
     ngOnInit() {
+        this.loadAll();
         this.principal.identity().then((account) => {
-            this.account = account;
+            this.currentAccount = account;
         });
-        this.registerAuthenticationSuccess();
+        this.registerChangeInMenus();
     }
 
-    registerAuthenticationSuccess() {
-        this.eventManager.subscribe('authenticationSuccess', (message) => {
-            this.principal.identity().then((account) => {
-                this.account = account;
-            });
-        });
+    ngOnDestroy() {
+        this.eventManager.destroy(this.eventSubscriber);
     }
 
-    isAuthenticated() {
-        return this.principal.isAuthenticated();
+    trackId(index: number, item: MenuCs) {
+        return item.id;
+    }
+    registerChangeInMenus() {
+        this.eventSubscriber = this.eventManager.subscribe('menuListModification', (response) => this.loadAll());
     }
 
-    login() {
-        this.modalRef = this.loginModalService.open();
+    private onError(error) {
+        this.jhiAlertService.error(error.message, null, null);
     }
 }
